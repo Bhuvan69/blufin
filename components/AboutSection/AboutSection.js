d074cae7e51de4693097c4600c6eaa3a6178bc10
Bhuvan <bhuvan@Bhuvans-MacBook-Pro.local>
2022-05-19 13:36:39 +0530

    winow resize issue fix

--- components/AboutSection/AboutSection.js
+++ components/AboutSection/AboutSection.js
@@ -1,10 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import CommonHeader from "../CommonHeader/CommonHeader";
 import styles from "./AboutSection.module.scss";
 import VisionMission from "./VisionMission";
 
 const AboutSection = () => {
+  const [size, setSize] = useState();
+
+  useEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener("resize", updateSize);
+    updateSize();
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+
   useEffect(() => {
     const element = document.getElementById("main-container");
     const style = element.currentStyle || window.getComputedStyle(element);
@@ -13,7 +24,7 @@ const AboutSection = () => {
     secondSection.style.marginRight = style.marginRight;
     secondSection.style.paddingLeft = style.paddingLeft;
     secondSection.style.paddingRight = style.paddingRight;
-  }, []);
+  }, [size]);
 
   return (
     <div className={styles.aboutSection}>
